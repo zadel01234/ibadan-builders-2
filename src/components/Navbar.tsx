4cf6a90f1705fd725d0b61427009154f236e3492
Olufayo Emmanuel <olufalzy01@gmail.com>
2026-04-26 09:14:29 +0100

    nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
         <div className="container">
           <a href="#home" className="nav-logo">
             <div className="nav-logo-mark">
-              <svg viewBox="0 0 24 24"><path d="M3 9l9-7 9 7v11a2 2 0 01-2 2H5a2 2 0 01-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
+              <svg viewBox="0 0 24 24"><polyline points="16 18 22 12 16 6" /><polyline points="8 6 2 12 8 18" /></svg>
             </div>
             <div>
               <div className="nav-logo-name">Ibadan Builders</div>
